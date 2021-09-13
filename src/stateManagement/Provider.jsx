0aa72ecb2c225a4cc0b91c4ef635b4ab7bd389ed
gallardoMG <gallartemx@gmail.com>
2021-09-13 15:53:00 -0500

    test-brach

--- src/stateManagement/Provider.jsx
+++ src/stateManagement/Provider.jsx
@@ -1,16 +1,19 @@
 import { createContext, useState } from 'react';
 
 export const Provider = ({ children }) => {
-    const [screenFormula, setScreenFormula] = useState([]);
-    const [screenOutput, setScreenOutput] = useState(['0']);
-    const values = {
-        screenFormula: screenFormula,
-        setScreenFormula: setScreenFormula,
-        screenOutput: screenOutput,
-        setScreenOutput: setScreenOutput,
-    };
-    return (
-        <Context.Provider value={{ ...values }}>{children}</Context.Provider>
-    );
+  const [screenFormula, setScreenFormula] = useState([]);
+  const [screenOutput, setScreenOutput] = useState(['0']);
+  const values = {
+    screenFormula: screenFormula,
+    setScreenFormula: setScreenFormula,
+    screenOutput: screenOutput,
+    setScreenOutput: setScreenOutput,
+  };
+  return (
+    <>
+      <Context.Provider value={{ ...values }}>{children}</Context.Provider>
+      {console.log('prueba con git')}
+    </>
+  );
 };
 export const Context = createContext();
